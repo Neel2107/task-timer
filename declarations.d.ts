b8d3074c570c59cbba212c64d2821cecd500a0ff
Neel <neelx2107@gmail.com>
2024-12-24 19:29:56 +0530

    chore: reset project & dependecies install

---
+++ declarations.d.ts
@@ -0,0 +1,24 @@
+declare module "*.svg" {
+    import React from "react";
+  import { SvgProps } from "react-native-svg";
+    const content: React.FC<SvgProps>;
+    export default content;
+  }
+
+
+
+  declare module '*.ttf' {
+    const content: string;
+    export default content;
+  }
+  declare module '@env' {
+    export const ENV: string;
+  }
+  declare module "*.png" {
+    const content: any;
+    export default content;
+}
+declare module "*.jpg" {
+  const content: any;
+  export default content;
+}
